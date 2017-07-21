9a7a08370320148526c49d2e3ace408fa955de58
joseamaya1 <joseamaya12331@gmail.com>
2017-07-21 16:44:12 -0700

    Update vip.lua

--- data/actions/scripts/other/vip.lua
+++ data/actions/scripts/other/vip.lua
@@ -1,2 +1,17 @@
-209.239.123.181
-olymp.sytes.net
+function onUse(cid, item, frompos, item2, topos)
+ 
+if item.itemid == 12666 then
+if getPlayerLevel(cid) > 1 then
+getPlayerStorageValue(cid, 11551)
+doSendAnimatedText(getPlayerPosition(cid), "Welcome!", TEXTCOLOR_RED)
+doCreatureSay(cid, "CONGRATULATIONS! You are now a VIP! You can now enter the VIP-area and use unique features!. ", TALKTYPE_ORANGE_1)
+setPlayerStorageValue(cid, 11551, 1)
+doRemoveItem(item.uid, 1)
+else
+doPlayerSendCancel(cid,'You are not a donator.')
+doRemoveItem(item.uid, 1)
+end
+else
+end
+return 1
+end
